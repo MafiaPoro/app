752fb385813bffe1e3cbe08e6d8e262debc797b5
MafiaPoro <p.iacob@ymail.com>
2018-04-23 23:50:50 +0100

    Fix

--- authen_login.php
+++ authen_login.php
@@ -1,29 +1,30 @@
 <?php
- require('db_connect.php');
+  require('db_connect.php');
 
-if (isset($_POST['StudID']) and isset($_POST['password'])){
+  if (isset($_POST['StudID']) and isset($_POST['password']))
+  {
+    // Assigning POST values to variables.
+    global $username;
+    $username = $_POST['StudID'];
+    $password = $_POST['password'];
 
-// Assigning POST values to variables.
-global $username;
-$username = $_POST['StudID'];
-$password = $_POST['password'];
+    // CHECK FOR THE RECORD FROM TABLE
+    $query = "SELECT * FROM `students` WHERE StudID='$username' and Password='$password'";
 
-// CHECK FOR THE RECORD FROM TABLE
-$query = "SELECT * FROM `students` WHERE StudID='$username' and Password='$password'";
+    $result = mysqli_query($connection, $query) or die(mysqli_error($connection));
+    $count = mysqli_num_rows($result);
 
-$result = mysqli_query($connection, $query) or die(mysqli_error($connection));
-$count = mysqli_num_rows($result);
-
-if ($count == 1){
-
-//echo "Login Credentials verified";
-echo "<script type='text/javascript'>alert('Login Credentials verified')</script>";
-require('/LoggedIn/index.php');
-//header( 'Location: LoggedIn');
-
-}else{
-echo "<script type='text/javascript'>alert('Invalid Login Credentials')</script>";
-//echo "Invalid Login Credentials";
-}
-}
+    if ($count == 1)
+    {
+      //echo "Login Credentials verified";
+      echo "<script type='text/javascript'>alert('Login Credentials verified')</script>";
+      require('LoggedIn.php');
+      //header( 'Location: LoggedIn');
+    }
+    else
+    {
+      echo "<script type='text/javascript'>alert('Invalid Login Credentials')</script>";
+      //echo "Invalid Login Credentials";
+    }
+  }
 ?>
